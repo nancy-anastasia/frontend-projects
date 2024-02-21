5bdb839dde0bde77a89c048e10aa0ede3fcb79a1
naku2300 <naku2300@student.miun.se>
2024-02-21 11:50:31 +0100

    built game card

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '6552c801970c4a1aa706ce9069c69da5'
+        key: '46d10068d12a4cceaa05f9cab8639460'
     }
 })
