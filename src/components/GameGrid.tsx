38c005b8d6f612e4ff175d9797a8516890db0f44
naku2300 <naku2300@student.miun.se>
2024-02-22 12:02:14 +0100

    implemented displaying critic score

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,13 +3,16 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 
 const GameGrid = () => {
-
-  const {games, error} = useGames();
+  const { games, error } = useGames();
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='10px' spacing={10}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -6,9 +6,9 @@ import {
   FaLinux,
   FaAndroid,
 } from "react-icons/fa";
-import { MdPhoneIphone } from 'react-icons/md';
-import { SiNintendo } from 'react-icons/si';
-import { BsGlobe } from 'react-icons/bs';
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 import { Platform } from "../hooks/useGames";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
@@ -18,22 +18,22 @@ interface Props {
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-    const iconMap: {[key: string]: IconType} = {
-        pc: FaWindows,
-        playstation: FaPlaystation,
-        xbox: FaXbox,
-        nintendo: SiNintendo,
-        mac: FaApple,
-        linux: FaLinux,
-        ios: MdPhoneIphone,
-        web: BsGlobe,
-        android: FaAndroid
-}
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
 
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-          <Icon as={iconMap[platform.slug]} color='gray.500'/>
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
