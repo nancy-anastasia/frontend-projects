a73e8d75f9a055eb7b0de5b6188b67202cb8ac6f
naku2300 <naku2300@student.miun.se>
2024-02-22 20:10:30 +0100

    updated critic score

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,9 +5,18 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-    let color = score > 85 ? 'green' : score > 60 ? 'yellow' : '';
+  let color = score >= 90 ? "green" : score > 70 ? "yellow" : "";
 
-    return <Badge colorScheme={color} fontSize='14px' paddingX='10px' borderRadius='4px'>{score}</Badge>;
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize="14px"
+      paddingX="10px"
+      borderRadius="4px"
+    >
+      {score}
+    </Badge>
+  );
 };
 
 export default CriticScore;
