d204f0203e12405097b110fded34b17435dc3ff0
naku2300 <naku2300@student.miun.se>
2024-03-04 15:44:14 +0100

    added comments to api-client.ts and image-url.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// Creating a custom Axios instance for making HTTP requests to the RAWG Database API
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '46d10068d12a4cceaa05f9cab8639460'
-    }
-})
+  baseURL: "https://api.rawg.io/api", // Base URL for all requests to the RAWG API
+  params: {
+    key: "46d10068d12a4cceaa05f9cab8639460", // API key for authenticating requests
+  },
+});
