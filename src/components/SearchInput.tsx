8a4d28a47662209b8464d47cac7a18d0877cd252
naku2300 <naku2300@student.miun.se>
2024-03-04 17:42:13 +0100

    added comments to SearchInput

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,20 +2,24 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
+// Interface defining the props expected by the SearchInput component
 interface Props {
-  onSearch: (searchText: string) => void;
+  onSearch: (searchText: string) => void; // Function to handle search submissions
 }
 
+// Component for a search input field, allowing users to search for games
 const SearchInput = ({ onSearch }: Props) => {
-  const ref = useRef<HTMLInputElement>(null);
+  const ref = useRef<HTMLInputElement>(null); // useRef hook to access the input element
 
   return (
+    // Wrapping the input in a form element to handle form submissions
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) onSearch(ref.current.value);
+        if (ref.current) onSearch(ref.current.value); // If the input is not null, call onSearch with its current value
       }}
     >
+      {/* Input group from Chakra UI to add an icon to the input field */}
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
         <Input
