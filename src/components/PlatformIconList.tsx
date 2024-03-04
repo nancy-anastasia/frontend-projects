23e7f1ad50cfcc4434545e2d5b83f67cb5f5f9e3
naku2300 <naku2300@student.miun.se>
2024-03-04 17:34:53 +0100

    added comments to PlatformIconList

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -13,11 +13,14 @@ import { Platform } from "../hooks/useGames";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
+// Defining interface for the component's props
 interface Props {
-  platforms: Platform[];
+  platforms: Platform[]; // Array of platforms associated with a game
 }
 
+// Component that renders a list of platform icons based on the game's available platforms
 const PlatformIconList = ({ platforms }: Props) => {
+  // Mapping platform slugs to their corresponding icons
   const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
@@ -31,6 +34,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   };
 
   return (
+    // Horizontal stack to align platform icons
     <HStack marginY={1}>
       {platforms.map((platform) => (
         <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
