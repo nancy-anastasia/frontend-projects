56532aeb7c36af28063afc422bcb027caafcc58d
naku2300 <naku2300@student.miun.se>
2024-03-04 17:28:35 +0100

    added comments to GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -10,16 +10,18 @@ import {
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
+// Interface defining the props expected by the GenreList component
 interface Props {
-  onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  onSelectGenre: (genre: Genre) => void; // Function to handle genre selection
+  selectedGenre: Genre | null; // The currently selected genre, if there is one
 }
 
+// Component to display a list of game genres, which allows the user to select one
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
-  const { data, isLoading, error } = useGenres();
+  const { data, isLoading, error } = useGenres(); // Fetching genre data using the custom hook
 
-  if (error) return null;
-  if (isLoading) return <Spinner />;
+  if (error) return null; // If there is an error fetching genres, do not render anything
+  if (isLoading) return <Spinner />; // Show a loading spinner while genre data is being fetched
 
   return (
     <>
@@ -30,12 +32,14 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
+              {/* Displaying a cropped image for each genre */}
               <Image
                 boxSize="32px"
                 borderRadius={8}
                 objectFit="cover"
                 src={getCroppedImageUrl(genre.image_background)}
               />
+              {/* Button to select a genre. It is highlighted if it's a currently selected genre */}
               <Button
                 whiteSpace="normal"
                 textAlign="left"
