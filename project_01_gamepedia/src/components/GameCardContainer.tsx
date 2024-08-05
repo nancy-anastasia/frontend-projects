2df2501f1d70a8229b7ef3ca92bf095054632934
naku2300 <naku2300@student.miun.se>
2024-08-05 16:37:09 +0200

    fixed card shadows and background in dark mode

--- project_01_gamepedia/src/components/GameCardContainer.tsx
+++ project_01_gamepedia/src/components/GameCardContainer.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 // Defining the interface for the component's props
@@ -8,9 +8,23 @@ interface Props {
 
 // Component for wrapping game card elements and providing consistent styling in the application
 const GameCardContainer = ({ children }: Props) => {
+  const { colorMode } = useColorMode(); // Color mode through ChakraProvider
+
+  const boxShadowMode =
+    colorMode === "dark"
+      ? "0 10px 20px rgba(0, 0, 0, 0.9), 0 6px 6px rgba(0, 0, 0, 0.7)"
+      : "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)";
+
+  const backgroundColor = colorMode === "dark" ? "#333333" : "white";
+
   return (
     // Using Chakra UI's Box component as a stylized container
-    <Box borderRadius={10} overflow="hidden">
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      boxShadow={boxShadowMode}
+      bg={backgroundColor}
+    >
       {children}
     </Box>
   );
