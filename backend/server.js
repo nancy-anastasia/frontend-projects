e3f485e0bd77504deb50885f42aec629e2bbcbe4
naku2300 <naku2300@student.miun.se>
2024-08-07 21:19:54 +0200

    added routes

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,18 @@
 require("dotenv").config();
 const express = require("express");
 const initializeDatabase = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
 
 // Call the function to initialize the database connection
 initializeDatabase();
 
 // Create an instance of an Express app
 const app = express();
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+// Route all requests to /api/products through the productRoutes module
+app.use("/api/products", productRoutes);
 
 // Define the port, defaulting to 5001 if not specified in environment variables
 const PORT = process.env.PORT || 5001;
