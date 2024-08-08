03bd54533003414bc7fbf9f046e0303ff7ab86ab
naku2300 <naku2300@student.miun.se>
2024-08-08 13:44:27 +0200

    added home icon and fixed hamburger menu

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,27 +4,28 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      {/* Logo */}
+      <div className="hamburger-menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
       <div className="navbar__logo">
         <h2>UrbanGear Shop</h2>
         <p>React, React Router, Redux and Node.js</p>
       </div>
       <ul className="navbar__links">
         <li>
+          <Link to="/">
+            <i className="fa-solid fa-house home-icon"></i>
+          </Link>
+        </li>
+        <li>
           <Link to="/cart" className="cart__link">
             <i className="fas fa-shopping-cart"></i>
             <span className="cart__count">0</span>
           </Link>
         </li>
-        <li>
-          <Link to="/">Shop</Link>
-        </li>
       </ul>
-      <div className="hamburger-menu">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
     </nav>
   );
 };
