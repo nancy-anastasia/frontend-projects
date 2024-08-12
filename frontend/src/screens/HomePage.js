2e2ef235e7145837b564cefc4f98260d7d0d24b3
naku2300 <naku2300@student.miun.se>
2024-08-12 10:41:56 +0200

    added basic homepage styles

--- frontend/src/screens/HomePage.js
+++ frontend/src/screens/HomePage.js
@@ -1,7 +1,12 @@
 import "./HomePage.css";
 
 const HomePage = () => {
-  return <div className="homepage">HomePage</div>;
+  return (
+    <div className="homepage">
+      <h2 className="homepage__title">Latest Products</h2>
+      <div className="homepage__products">{/* <Product /> */}</div>
+    </div>
+  );
 };
 
 export default HomePage;
