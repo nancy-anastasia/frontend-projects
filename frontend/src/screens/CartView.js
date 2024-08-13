3edb5f9b95181e8b1612d1c8a7e9c8f125591dae
naku2300 <naku2300@student.miun.se>
2024-08-13 23:06:35 +0200

    added markup in cartview

--- frontend/src/screens/CartView.js
+++ frontend/src/screens/CartView.js
@@ -1,7 +1,24 @@
 import "./CartView.css";
+import CartItem from "../components/CartItem";
 
 const CartView = () => {
-  return <div className="cartview">CartView</div>;
+  return (
+    <div className="cartview">
+      <div className="cartview__left">
+        <h2>Shopping Cart</h2>
+        <CartItem />
+      </div>
+      <div className="cartview__right">
+        <div className="cartview__info">
+          <p>Subtotal (0) items</p>
+          <p>$499.99</p>
+        </div>
+        <div>
+          <button>Proceed to Checkout</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartView;
