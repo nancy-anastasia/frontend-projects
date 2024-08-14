47c57f8939b3d26174737cf41be4a7b2a443408c
naku2300 <naku2300@student.miun.se>
2024-08-14 22:49:33 +0200

    updated store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,37 +1,20 @@
-/* This project uses Redux Toolkit's configureStore instead of manually creating 
-   the store with createStore. configureStore automatically sets up Redux DevTools, 
-   applies middleware, and combines reducers, making it the recommended way 
-   to configure the Redux store in modern applications.
- */
 /* Up-to-date dependencies can be installed by running 
     "npm install @reduxjs/toolkit react-redux"
- */
-
-// "configureStore" replaces "createStore" and simplifies store setup
-import { configureStore } from "@reduxjs/toolkit";
-
-// Importing thunk for handling asynchronous actions
-import { thunk } from "redux-thunk";
-
-/* 
-  If you're only using redux-thunk without any additional custom middleware, 
-  you can omit the import of 'redux-thunk' entirely, as 'configureStore' 
-  automatically includes it by default. However, if you need to add other custom 
-  middleware or modify the default middleware configuration, 
-  then including it like this is necessary.
 */
 
-// Import combined reducers from the "reducers" file
-import rootReducer from "./reducers/reducers";
+/* Configuration for the Redux store using Redux Toolkit, which enhances setup simplicity 
+  and reliability by automatically configuring middleware and enabling Redux DevTools in development  environments.
+ */
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { cartReducer } from "./reducers/cartReducers"; // Importing the cartReducer for the cart state management
 
-// Middleware setup to handle asynchronous actions (e.g., API calls)
-const middleware = [thunk];
+const reducer = combineReducers({
+  cart: cartReducer, // Combines the cartReducer into the root reducer
+});
 
+// Configure and export the Redux store. Default middleware from Redux Toolkit includes thunk.
 const store = configureStore({
-  reducer: rootReducer, // Combines all reducers into one root reducer
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleware), // Adds redux-thunk and any other custom middleware to the default ones
-  devTools: process.env.NODE_ENV !== "production", // Enables Redux DevTools only in development mode environments
+  reducer, // Apply the combined reducers to the store
 });
 
 export default store;
