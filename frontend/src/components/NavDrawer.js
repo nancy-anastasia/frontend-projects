b12673171a4220e63b071280113de79ecdb349f1
naku2300 <naku2300@student.miun.se>
2024-08-15 11:41:57 +0200

    fixed classNames in NavDrawer

--- frontend/src/components/NavDrawer.js
+++ frontend/src/components/NavDrawer.js
@@ -13,19 +13,19 @@ const NavDrawer = ({ isVisible, navDrawerClick }) => {
       <ul className="nav-drawer__links" onClick={navDrawerClick}>
         <li>
           <Link>
-            <i class="fa-solid fa-xmark close-mark"></i>
+            <i className="fa-solid fa-xmark close-mark"></i>
             <p className="nav-drawer__shop-logo">UrbanGear Shop</p>
           </Link>
         </li>
         <hr className="nav-drawer__divider" />
         <li className="nav-drawer__item">
           <Link to="/cart">Shopping Cart</Link>
-          <i class="fa-solid fa-chevron-right"></i>
+          <i className="fa-solid fa-chevron-right"></i>
         </li>
         <hr className="nav-drawer__divider" />
         <li className="nav-drawer__item">
           <Link to="/">Home</Link>
-          <i class="fa-solid fa-chevron-right"></i>
+          <i className="fa-solid fa-chevron-right"></i>
         </li>
         <hr className="nav-drawer__divider" />
       </ul>
