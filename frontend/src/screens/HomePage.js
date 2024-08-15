fff0061b514b5e26d1ad8ab1a09af25d169d5383
naku2300 <naku2300@student.miun.se>
2024-08-15 11:10:54 +0200

    added product loading to homepage

--- frontend/src/screens/HomePage.js
+++ frontend/src/screens/HomePage.js
@@ -1,19 +1,33 @@
 import "./HomePage.css";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Import components and actions
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import { getProductsData as productsList } from "../store/actions/productActions";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+
+  const getProducts = useSelector((state) => state.getProductsData);
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(productsList());
+  }, [dispatch]);
+
   return (
     <div className="homepage">
       <h2 className="homepage__title">Latest Products</h2>
       <div className="homepage__products">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <h2>{error}</h2>
+        ) : (
+          products.map((product) => <Product />)
+        )}
       </div>
     </div>
   );
